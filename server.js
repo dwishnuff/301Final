53404d92e1f3826924be4ea7ef9a9d2d663968d4
J3ffcon1 <Jrlone1990@gmail.com>
2017-12-07 19:49:39 -0800

    added new client to connect to postgres, called database table.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 
-const conString = '';
+const conString = ''; //constring to connect to
+
+const client = new pg.Client(conString); //create new client that will connect with postgres.
+
+client.connect(); //connecting method.
+
 
 function loadDB() {
   client.query(`
@@ -9,5 +14,7 @@ CREATE TABLE IF NOT EXISTS
   img_source VARCHAR(255),
   operator VARCHAR(255),
   position INTEGER,
-    `)
+    `) //creating table with properties and values.
 };
+
+loadDB(); //calling table.
