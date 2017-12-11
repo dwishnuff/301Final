887380a8369ff67e3508b89b5ebe0e0d49b10306
Heather Knight <heather.knight.pdx@gmail.com>
2017-12-10 17:31:36 -0800

    search image function now works, however, there is a sizing issue.  Will address next

--- public/scripts/iconselect.js
+++ public/scripts/iconselect.js
@@ -1,12 +1,20 @@
 $('#searchForm').submit((event)=>{
   event.preventDefault();
   const userInput=$('input[name="search"]').val();
-  nounProject.getIconsByTerm(userInput, {limit: 10}, function (err, data) {
-      if (err) console.log(err);
-      $.each(data.icons, (icon)=>{
-        $('#photoSelector').append(`<img src="${icon.preview_url}">`);
+  $.post('/searchIcons', { searchTerm: userInput})
+   .then(icons=>{
+     icons.forEach((icon)=>{
+       console.log(icon.preview_url)
+      $('#photoSelector').append(`<img src="${icon.preview_url}">`);
       });
+   })
+
+  // nounProject.getIconsByTerm(userInput, {limit: 10}, function (err, data) {
+  //     if (err) console.log(err);
+  //     $.each(data.icons, (icon)=>{
+  //       $('#photoSelector').append(`<img src="${icon.preview_url}">`);
+  //     });
    });
-})
+
 
 //search icon, select icon, select operator, repeat.  Add link. Message.
