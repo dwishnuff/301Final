1641ec527d8d72fd8e9d295c46d6fd6e6a3e51cd
J3ffcon1 <Jrlone1990@gmail.com>
2017-12-11 20:55:47 -0800

    added template files/radio buttons

--- public/scripts/previewView.js
+++ public/scripts/previewView.js
@@ -1,7 +1,7 @@
 //JQUERY change for radio buttons on preview.HTML
-
+//think about using iframe html in order to change one aspect of your code.
 $('input[type=radio]').change(function(e) {
   console.log(e.target.value);
-  
+
 
 })
