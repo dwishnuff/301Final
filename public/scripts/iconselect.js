006f67a7f8b9dd0a09c08d02ffda30aa785e8833
Danielle Beyer <dwishnuff@gmail.com>
2017-12-12 21:02:04 -0800

    updated iconselect.js functions and added operator styling

--- public/scripts/iconselect.js
+++ public/scripts/iconselect.js
@@ -2,34 +2,34 @@ $('#searchForm').submit((event)=>{
   event.preventDefault();
   const userInput=$('input[name="search"]').val();
   $.post('/searchIcons', { searchTerm: userInput})
-   .then(icons=>{
-     icons.forEach((icon)=>{
-       console.log(icon.preview_url)
-      $('#photoSelector').append(`<img src="${icon.preview_url}">`);
-      });
-   });
+  .then(icons=>{
+    icons.forEach((icon)=>{
+      console.log(icon.preview_url)
+      $(`<img src="${icon.preview_url}">`).click(recordIconClick).appendTo('#photoSelector');
+    });
   });
+});
 
 //var to hold SelectedOptions
 var message = [];
 
 var iconClicked
 var operatorClicked
-  //constructor function
+//constructor function
 
 
 //onClick event to highlight clicked icon
 function recordIconClick(event) {
-  iconClicked = event.target.icon.preview_url;
+  iconClicked = event.target.getAttribute("src");
   event.target.classList.add("onClick");
-  message.push(iconClicked)
+  // message.push(iconClicked)
 }
 
 //Select Operator
 function recordOperatorClick(event) {
   operatorClicked = event.target.operator;
   event.target.classList.add("onClick");
-  message.push(operatorClicked)
+  // message.push(operatorClicked)
 };
 //Add to Message
 
@@ -38,10 +38,10 @@ function addToMessage(event) {
     iconURL: iconClicked,
     operator: operatorClicked
   };
-  message.push(newPair);
-}
+  message.push(newPair)
+};
 
-$(".iconAdd").click(function(addToMessage))
+$("#iconAdd").click(addToMessage);
 
 
 //search icon, select icon, select operator, repeat.  Add link. Message.
