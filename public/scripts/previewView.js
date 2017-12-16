5f22fe451ce3bf2e77818caa646c501ee2a43f96
Jeff <jrlone1990@gmail.com>
2017-12-16 13:23:47 -0800

    Revert "Template branch"

--- public/scripts/previewView.js
+++ public/scripts/previewView.js
@@ -5,25 +5,15 @@ $('input[type=radio]').change(function(e) {
   $('#prettyPreview').contents().find('link[rel="stylesheet"]').attr("href",`styles/templates/${e.target.value}.css`);
 
 })
-
-//lab 8 server.js $.put(/articles) example follow that, you'll want to update your table with the templace_css.
 //use JQuery.post([settings])
 //save the result to the database on preview.html.
-$.post('/meyou', function(request, response){
-  console.log()
-
-});
+$.post(url, [data], [callback],[type] )
 
 
 //use JQuery.get([settings])
 //load the result that was saved in message.html.
 
 $.get('/meyou', function(request, response){
-  client.query('SELECT template_css FROM greeting')
-  .then(function(result) {
-    response.send(result.rows);
-  })
-  .catch(function(err) {
-    console.error(err)
-  })
-},)
+client.query('SELECT template_css FROM meyou')
+
+}, [success], [dataType])
